c1bcde1ac568883deca0c2ae04f97fea07d00869
ArturKrush <arturkrush@gmail.com>
2025-11-01 15:30:21 +0200

    init: CalculatorApp and CalculatorLib empty projects

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using CalculatorLib;
+using System;
+using System.Text;
+
+namespace CalculatorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+    }
+}
